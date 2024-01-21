5486294ae68a0d50bbeb5bfdc74fb80b2a801396
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-01-21 16:00:14 -0300

    Feat: Creating in memory dbs.

---
+++ desafio-backend-1/DataBase/ClienteDB.cs
@@ -0,0 +1,32 @@
+﻿using desafio_backend_1.Models.Clientes;
+
+namespace desafio_backend_1.DataBase
+{
+    internal static class ClienteDB
+    {
+        private static List<Cliente> _clientes;
+
+        public static List<Cliente> Clientes
+        {
+            get
+            {
+                return _clientes ?? new List<Cliente>();
+            }
+        }
+
+        public static void Add(Cliente cliente)
+        {
+            _clientes.Add(cliente);
+        }
+
+        public static void Edit(int id, Cliente novoCliente)
+        {
+            var cliente = _clientes.SingleOrDefault(x => x.Id == id) ??
+                throw new ArgumentNullException("There is no Cliente with this Id");
+
+            _clientes.Remove(cliente);
+
+            Add(novoCliente);
+        }
+    }
+}
---
+++ desafio-backend-1/DataBase/ProblemaDeSaudeDB.cs
@@ -0,0 +1,32 @@
+﻿using desafio_backend_1.Models.Clientes;
+
+namespace desafio_backend_1.DataBase
+{
+    internal static class ProblemaDeSaudeDB
+    {
+        private static List<ProblemaDeSaude> _problemasDeSaude;
+
+        public static List<ProblemaDeSaude> ProblemasDeSaude
+        {
+            get
+            {
+                return _problemasDeSaude ?? new List<ProblemaDeSaude>();
+            }
+        }
+
+        public static void Add(ProblemaDeSaude problema)
+        {
+            _problemasDeSaude.Add(problema);
+        }
+
+        public static void Edit(int id, ProblemaDeSaude problema)
+        {
+            var problemaDeSaude = _problemasDeSaude.SingleOrDefault(x => x.Id == id) ??
+                throw new ArgumentNullException("There is no ProblemaDeSaude with this Id");
+
+            _problemasDeSaude.Remove(problemaDeSaude);
+
+            Add(problema);
+        }
+    }
+}
