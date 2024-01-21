16c4766c3499bbe706de5f67835bf6d699df29ee
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-01-21 15:31:56 -0300

    Feat: Adding primary construtor to ProblemasDeSaude entity.

--- desafio-backend-1/Models/Clientes/ProblemaDeSaude.cs
+++ desafio-backend-1/Models/Clientes/ProblemaDeSaude.cs
@@ -2,14 +2,14 @@
 
 namespace desafio_backend_1.Models.Clientes
 {
-    public class ProblemaDeSaude
+    public class ProblemaDeSaude(int id, string nome, EnumGrauDoProblema grauDoProblema, int clienteId)
     {
-        public int Id { get; set; }
+        public int Id { get; set; } = id;
 
-        public string Nome { get; set; }
+        public string Nome { get; set; } = nome;
 
-        public EnumGrauDoProblema GrauDoProblema { get; set; }
+        public EnumGrauDoProblema GrauDoProblema { get; set; } = grauDoProblema;
 
-        public int ClienteId { get; set; }
+        public int ClienteId { get; set; } = clienteId;
     }
 }
